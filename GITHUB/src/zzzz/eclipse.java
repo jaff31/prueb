295a4996153ae9b7eb86c6bc4f9aa09533b61411
jaff31 <javierayllonfeijoo@gmail.com>
2024-02-09 12:09:02 +0100

    bash commit

--- GITHUB/src/zzzz/eclipse.java
+++ GITHUB/src/zzzz/eclipse.java
@@ -3,7 +3,7 @@ package zzzz;
 public class eclipse {
 
 	public static void main(String[] args) {
-		System.out.println("Hola desde eclipse");
+		System.out.println("Hola desde visual");
 	}
 
 }
--- git/src/proyecto1/prueba.java
+++ git/src/proyecto1/prueba.java
@@ -3,7 +3,9 @@ package proyecto1;
 public class prueba {
 
 	public static void main(String[] args) {
-		System.out.println("Hola ");
+		
+		System.out.println("Hola desde eclipse");
+
 	}
 
 }
